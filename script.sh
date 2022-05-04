1b38210ef578ffe1706d0ca12424983931155f27
xLarryPHP <104874114+xLarryPHP@users.noreply.github.com>
2022-05-04 16:24:56 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,10 @@ sudo yum install httpd -y
 sudo systemctl enable httpd.service
 sudo systemctl start httpd.service
 sudo yum install php php-mysql -y
-sudo systemctl restart httpd.service -y
+sudo systemctl restart httpd.service
+yum install epel-release -y
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install yum-utils -y
+yum-config-manager --enable remi-php72 -y
+yum update -y
+yum install php72 -y
